cee9e2238e461335df17d41b21bcf6714c86e358
Silvia Mollica <silvia.mollica18@gmail.com>
2020-11-04 13:01:16 +0100

    Primo Commit

---
+++ src/main/java/Prova/EseIngSw/MyListUtil.java
@@ -0,0 +1,36 @@
+package Prova.EseIngSw;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MyListUtil {
+
+public List<Integer> NumberList(int n)
+ {
+	Scanner imput = new Scanner(System.in);
+	
+	System.out.println("Scegli tra Crescente o Decrescente");
+	String Scelta = imput.nextLine();
+	
+	
+	System.out.println("Inserisci 5 numeri a casa sa 1 a 10");
+	List<Integer> List = new LinkedList<>();
+	
+	if(Scelta.equals("Descrescente")) {
+		
+	}
+	
+	if(Scelta.equals("Descrescente")) {
+		
+	}
+	return List;
+		 
+	 
+ }  
+
+ public void Ordinato() {
+	 
+ }
+
+}
---
+++ src/test/java/Prova/EseIngSw/MyListUtilTest.java
@@ -0,0 +1,5 @@
+package Prova.EseIngSw;
+
+public class MyListUtilTest {
+
+}
